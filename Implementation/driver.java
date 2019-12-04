075645052d57b651133b77bbf2f3d597d1f7ad64
Alex Pang <alex.pang9111@gmail.com>
2019-12-03 22:42:33 -0600

    driver

---
+++ Implementation/driver.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class driver {
+    public static void drive(ArrayList array){
+        if (array.get(0) == "commit"){
+            //call commit
+        }
+        if (array.get(0) == "checkout"){
+            Object filename = array.get(1);
+            Object version = array.get(2);
+            //call checkout with args
+        }
+        if (array.get(0) == "merge"){
+            Object filename = array.get(1);
+            Object version = array.get(2);
+            //call merge with args
+        }
+
+
+    }
+}

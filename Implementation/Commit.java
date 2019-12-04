b3fbcff5b9116030bb9d5a582b3eed87765f302b
seojinko <seojin-ko@uiowa.edu>
2019-12-03 23:35:15 -0600

    Modify Commit

--- Implementation/Commit.java
+++ Implementation/Commit.java
@@ -1,10 +1,21 @@
-public class Commit<T> extends DAG {
+public class Commit {
 
-   public DAG<T> revisionTree;
-   public Node<T> currentWorkingFile;
 
-   public commit(DAG<T> reviTree, Node<T> currFile) {
-       if (currFile.hasChildren())
+    // I think combining all methods together later would be better
+    // because we don't need to call DAG revisionTree all the time then
+    // but I will suppose we are not at this moment
+
+    public DAG reviTree;    // revision tree
+    public Node currFile;   // current working file
+
+   public void commit(DAG reviTree, Node currFile) {
+       if (currFile.getChildren().size() !=0 ) {
+           branch(reviTree, currFile);
+       }
+   }
+
+   public void branch(DAG reviTree, Node currFile) {
+       //currFile.revisionNumber.setNextRN();
    }
 
 }

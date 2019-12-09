7dcbfefc825dc552288537b0e6fdb16115ffa01b
Alex Pang <alex.pang9111@gmail.com>
2019-12-08 21:18:04 -0600

    Added dfsWrite to write to xml in dfs order. Changed write in StaxMaker

--- Implementation/staxMaker.java
+++ Implementation/staxMaker.java
@@ -9,6 +9,7 @@ public class staxMaker {
         D.add(D.getSentinel().getElem(),"1.1");
         D.add("1.1", "1.2");
         D.add("1.2", "1.3");
+        D.add("1.2","1.2.1");
         staxMaker s = new staxMaker("test", D);
         s.write();
     }
@@ -17,36 +18,54 @@ public class staxMaker {
         this.file = file;
         this.D = D;
     }
-
+    public void dfsWrite(Node version) throws IOException {
+        FileWriter fw = new FileWriter(this.file.split("\\.")[0] + ".xml", true); // true for appending option
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter pw = new PrintWriter(bw);
+        pw.println("<Node Version =\"" + version.getElem() + "\">");
+        pw.println("<Parent>" + version.getParent().getElem() + "</Parent>");
+        String branch = "F";
+        if (version.getIsBranch()){
+            branch = "T";}
+        pw.println("<isBranch>" + branch + "</isBranch>");
+        pw.println("<Children>");
+        for (Object child:version.getChildren()){
+            Node c = (Node)child;
+            pw.println("<Child>" + c.getElem() + "</Child>");
+            System.out.println("CHILD: " + c.getElem());
+            System.out.println("Its parent: " + c.getParent().getElem());
+        }
+        pw.println("</Children>");
+        pw.println("</Node>");
+        pw.close();
+        bw.close();
+        fw.close();
+        for (Object child: version.getChildren()) {
+            dfsWrite((Node) child);
+        }}
     public void write() throws IOException {
         File f = new File(this.file.split("\\.")[0] + ".xml");
-        Node cur = this.D.getSentinel();
+        Node cur = this.D.find("1.1");
         f.delete();
         FileWriter fw = new FileWriter(this.file.split("\\.")[0] + ".xml", true); // true for appending option
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter pw = new PrintWriter(bw);
         pw.println("<?xml version=\"1.0\"?>");
         pw.println("<FileName name=\"" + this.file + "\">");
-        while (cur.getChildren().size() > 0) {
-            Node next = (Node)cur.getChildren().get(0); // will be main branch child
-            pw.println("<Node Version =\"" + next.getElem() + "\">");
-            pw.println("<Parent>" + next.getParent().getElem() + "</Parent>");
-            pw.println("<Children>");
-            for (Object child:next.getChildren()){
-                Node c = (Node)child;
-                pw.println("<Child>" + c.getElem() + "</Child>");
-                System.out.println("CHILD: " + c.getElem());
-                System.out.println("Its parent: " + c.getParent().getElem());
-            }
-            pw.println("</Children>");
-            pw.println("</Node>");
-            System.out.println(next.getElem() + " successfully appended into file");
-            pw.flush();
-            cur = next;
-        }
+        pw.close();
+        bw.close();
+        fw.close();
+        dfsWrite(cur);
+        fw = new FileWriter(this.file.split("\\.")[0] + ".xml", true); // true for appending option
+        bw = new BufferedWriter(fw);
+        pw = new PrintWriter(bw);
+        System.out.println(cur.getElem() + " successfully appended into file");
+        pw.flush();
+        cur = cur;
         pw.println("</FileName>");
         pw.close();
         bw.close();
         fw.close();
-    }
-}
+
+    }}
+
